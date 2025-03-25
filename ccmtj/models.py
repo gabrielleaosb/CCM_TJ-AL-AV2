35db7f245062e2daa49ea7164cfe9fd0e36499d7
gabrielleaosb <gabriellsbagetti@gmail.com>
2025-03-25 19:44:29 -0300

    home page

--- ccmtj/models.py
+++ ccmtj/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class SelfieHistorica(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
-    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
+    imagem = models.ImageField(upload_to='thumbs/', blank=True, null=True)
 
     def __str__(self):
         return self.nome
--- ccmtj/views.py
+++ ccmtj/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'ccmtj/home.html')
+
+def selfie_historica(request):
+    return render(request, 'ccmtj/selfie.html')
