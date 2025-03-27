bec88ca94089fc92496042dab32a6fa57499f50e
gabrielleaosb <gabriellsbagetti@gmail.com>
2025-03-27 14:39:09 -0300

    selfie-page: Adiciona navegação de imagens e fixa navbar/footer

--- ccmtj/views.py
+++ ccmtj/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import SelfieHistorica
 
 def home(request):
     return render(request, 'ccmtj/home.html')
 
 def selfie_historica(request):
-    return render(request, 'ccmtj/selfie.html')
+    thumbs = SelfieHistorica.objects.all()
+    return render(request, 'ccmtj/selfie.html', {'thumbs': thumbs})
