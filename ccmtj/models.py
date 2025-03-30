8d2ffc9a723fa2dbf8e5f02a89a9945fde710ae1
gabrielleaosb <gabriellsbagetti@gmail.com>
2025-03-30 14:41:49 -0300

    att

--- ccmtj/models.py
+++ ccmtj/models.py
@@ -4,6 +4,7 @@ class SelfieHistorica(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
     imagem = models.ImageField(upload_to='thumbs/', blank=True, null=True)
+    personagemSelfie = models.ImageField(upload_to='personagemSelfie', blank=True, null=True)
 
     def __str__(self):
         return self.nome
